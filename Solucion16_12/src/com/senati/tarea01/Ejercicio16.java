d440a3e106e69e3371f239b1ef80f63aa526ddfd
Alumno <Alumno@DESKTOP-QK63PLK>
2019-12-17 09:57:16 -0500

    version 1.1

---
+++ Solucion16_12/src/com/senati/tarea01/Ejercicio16.java
@@ -0,0 +1,10 @@
+package com.senati.tarea01;
+
+public class Ejercicio16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Holas...");
+	}
+
+}
